67677c39b5703437d587aabf8ef53e84421b3958
landy8530 <landy8530@hotmail.com>
2018-03-17 11:08:21 +0800

    修改README.md

--- oracle/sqlloader/create_init_data.sql
+++ oracle/sqlloader/create_init_data.sql
@@ -41,8 +41,8 @@ END;
 
 whenever sqlerror exit -1;
 
--- create temp table ehtemp.EMAIL_APP_LEAD;
-create table ehtemp.EMAIL_APP_LEAD
+-- create temp table EMAIL_APP_LEAD;
+create table EMAIL_APP_LEAD
 (
   USER_PROFILE_ID         NUMBER,
   LEAD_ID                 NUMBER,
@@ -52,42 +52,6 @@ create table ehtemp.EMAIL_APP_LEAD
   EMAIL_FLAG              CHAR(2)
 )
 ;
-
-
--- create temp table ehtemp.EMAIL_SBG
-CREATE TABLE ehtemp.EMAIL_SBG
-(
-  USER_PROFILE_ID         NUMBER,
-  GROUP_ID                NUMBER,
-  EMAIL                   VARCHAR2(100),
-  CORRECTED_DO_NOT_MAIL   VARCHAR2(100),
-  EMAIL_FLAG              CHAR(2)
-)
-;
-
--- create temp table ehtemp.EMAIL_FLAG_STATUS
-CREATE TABLE ehtemp.EMAIL_FLAG_STATUS
-(
-  EMAIL_FLAG              CHAR(1), --WEBBULA STATUS
-  EMAIL_STATUS_LEAD       VARCHAR2(60),--LEAD STATUS
-  EMAIL_STATUS_USER       CHAR(1)  --USER_PROFILE STATUS
-)
-;
-
---create primary key
-ALTER TABLE ehtemp.EMAIL_FLAG_STATUS add constraint EMAIL_FLAG_STATUS_KEY primary key(EMAIL_FLAG);
-
--- LEAD STATUS:'Active', 'Bounced', 'Remove/Unsubscribe'
--- send_email in ('Y','N','B')
-INSERT INTO ehtemp.EMAIL_FLAG_STATUS VALUES('V','Active','Y');
-INSERT INTO ehtemp.EMAIL_FLAG_STATUS VALUES('U','Active','Y');
-INSERT INTO ehtemp.EMAIL_FLAG_STATUS VALUES('R','Bounced','B');
-INSERT INTO ehtemp.EMAIL_FLAG_STATUS VALUES('F','Bounced','B');
-INSERT INTO ehtemp.EMAIL_FLAG_STATUS VALUES('D','Bounced','B');
-INSERT INTO ehtemp.EMAIL_FLAG_STATUS VALUES('C','Bounced','B');
-INSERT INTO ehtemp.EMAIL_FLAG_STATUS VALUES('I','Bounced','B');
-
-COMMIT ;
 
 PROMPT
 set serveroutput on size 1000000
--- oracle/sqlloader/data.ctl
+++ oracle/sqlloader/data.ctl
@@ -1,14 +1,14 @@
-OPTIONS (skip=1,rows=128)
+OPTIONS (skip=1,rows=128) -- sqlldr 命令显示的选项可以写到这里边来,skip=1 用来跳过数据中的第一行
 LOAD DATA
-INFILE 'data.csv'
-BADFILE 'data.bad'
+INFILE 'data.csv' --指定外部数据文件，可以写多个 INFILE "another_data_file.csv" 指定多个数据文件
+BADFILE 'data.bad' --这里还可以使用 BADFILE、DISCARDFILE 来指定坏数据和丢弃数据的文件，
 DISCARDFILE 'data.dsc'
-TRUNCATE
-INTO TABLE EMAIL_APP_LEAD
-fields terminated by ','
-TRAILING NULLCOLS 
+TRUNCATE --操作类型，用 truncate table 来清除表中原有记录
+INTO TABLE EMAIL_APP_LEAD -- 要插入记录的表
+fields terminated by ','  -- 数据中每行记录用 "," 分隔
+TRAILING NULLCOLS  --表的字段没有对应的值时允许为空
 (
-  USER_PROFILE_ID ,
+  USER_PROFILE_ID ,--字段可以指定类型，否则认为是 CHARACTER 类型, log 文件中有显示
   LEAD_ID         ,
   APPLICATION_ID  ,
   EMAIL           ,
